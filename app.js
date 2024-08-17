66f8339503109384fa12d458744d582ffd9740cc
Aziz <azizatamuradov1994@gmail.com>
2024-08-17 19:56:26 -0400

    class

--- app.js
+++ app.js
@@ -1,18 +1,35 @@
-const button = document.getElementById('start');
+//  ///////// variables 
 
- 
-// button.addEventListener('click', function() {
-//     alert('hey')
-// });
-
- // Preventing the default action when a link is clicked
- 
-
-link.addEventListener('click', function(event) {
-    event.preventDefault(); // Prevents the browser from following the link
-    alert('Link click prevented!');
-});
+let currentRound = 1;         //// current round
+let horde = [];
+let p1Turn = true;
+let gameActive = false;
+const maxRounds = 5;   //// max numbers of round
+let ussAssembly = null;
 
 
+///// random number 
+
+const generateRandomNumber = (min, max) => Math.floor(Math.random() * (max - min)) + min;
+
+
+/////class represent player and aliens 
+
+
+class Player {
+    constructor(name, hull, firepower, accuracy ) {
+        this.name = name;  /// name of ship
+        this.hull = hull; /// health points of ship
+        this.firepower = firepower;  /// damage points the ship can deal
+        this.firepower = accuracy; /// probability of a successful attack
+    }
+    attack(target) {
+        return Math.random() < this.accuracy; /// true if attack hits , false otherwise
+    }
+}
+
+
+
+/// 
 
  
